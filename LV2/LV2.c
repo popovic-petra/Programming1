c2da5dba7f2c999d4a7fdba0e2f094f5e30cc075
popovic-petra <popovicpetra99@gmail.com>
2023-10-19 21:56:47 +0200

    Add project files.

---
+++ LV2/LV2.c
@@ -0,0 +1,81 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void zadatak1();
+void zadatak2();
+void zadatak3();
+void zadatak4();
+
+int main() {            // u int main() pozivam zadatak koji želim provjeriti
+    
+    zadatak1();
+
+    return 0;
+}
+
+
+void zadatak1() {
+
+    #define _CRT_SECURE_NO_WARNINGS
+	int a;
+	
+    printf("Unesite cijeli broj: ");
+	scanf("%d", &a);
+	
+    printf("Unijeli ste broj %d.\n", a);
+	
+    return 0;
+}
+
+void zadatak2() {
+
+    int a;
+    int b;
+
+    printf("Unesi dvije cjelobrojne vrijednosti!\n");
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    printf("%d %d", a, b);
+
+    return 0;
+}
+
+void zadatak3() {
+    
+    int visina;
+    int masa;
+
+    printf("Unesite svoju visinu u cm i masu u kilogramima!\n");
+
+    printf("Visina: ");
+    scanf("%d", &visina);
+
+    printf("Masa: ");
+    scanf("%d", &masa);
+
+    printf("Tvoja visina je %d cm, a tvoja masa je %d kg!", visina, masa);
+
+    return 0;
+}
+
+
+void zadatak4() {
+    int a;
+    int b;
+    int c;
+    
+    printf("Unesite duljinu prve stranice trokuta: ");
+    scanf("%d", &a);
+
+    printf("Unesite duljinu druge stranice trokuta (dvostruka veća od prethodne): ");
+    scanf("%d", &b);
+
+    printf("Unesite duljinu treće stranice trokuta (dvostruka veća od prethodne): ");
+    scanf("%d", &c);
+
+    printf("Stranice trokuta u obrnutom redoslijedu su: %d %d %d", c, b, a);
+
+    return 0;
+}
---
+++ LV3/LV2.c
@@ -0,0 +1,98 @@
+﻿// 1. ZADATAK
+
+
+/*
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	const int a = 1101;
+	char znak = 'A';
+	double broj;
+
+	printf("Unesite realni broj: ");
+	scanf("%lf", &broj);
+	printf("Unijeli ste: %f\n", broj);
+
+	return 0;
+} 
+*/
+
+
+// 2. ZADATAK
+
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	char znak1;
+	char znak2;
+
+	printf("Unesite znak: ");
+	scanf(" %c", &znak1);
+	printf("ASCII vrijednost znaka %c je %d.\n", znak1, znak1);
+
+	getchar();
+
+	printf("\nUnesite znak: ");
+	scanf(" %c", &znak2); // dodaka razmak prije %c kako bih spriječila program da uzima newline kao unos u scanf... ILI TO ILI getchar()!!
+
+	printf("ASCII vrijednost znaka %c je %d.\n", znak2, znak2);
+
+	return 0;
+}
+*/
+
+/*
+Ovaj pristup je koristan kada želite unjeti karaktere, ali želite ignorirati praznine (bijeline) 
+koje korisnik može slučajno unjeti. Na primjer, ako korisnik pritisne Enter nakon prvog unosa, 
+bez ovog razmaka, scanf bi odmah završio unos i uzima newline karakter kao drugi unos, 
+što često nije željen rezultat. Korištenjem razmaka, možete zaobići taj problem.
+*/
+
+// Umjesto praznine ispred %c mogli smo koristiti i funkciju getchar()
+
+// 3. ZADATAK
+/*
+
+int main(void) {
+	
+	int a = 90701;
+	float pi = 3.14;
+	double duzi_pi = 3.14159265395;
+	char znak = 'a';
+
+	printf("Tip int: cijeli broj %d zauzima %lu bajtova.\n", a, sizeof(int));
+	printf("Tip float: realni broj %f zauzima %lu bajtova.\n", pi, sizeof(float));
+	printf("Tip double: precizniji realni broj %f zauzima %lu bajtova.\n", duzi_pi, sizeof(double));
+	printf("Tip char: znak %c zauzima %lu bajtova.\n", znak, sizeof(char));
+
+	return 0;
+} 
+*/
+
+// 4. ZADATAK
+
+/* Kreirati na dva različita načina različite realne simboličke konstante napisane 
+znanstvenom notacijom. 
+Ispisati na ekran njihove vrijednosti i koliko bajta memorije zauzimaju.*/
+
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	long double brz_svj_u_vakuumu = 2.99792458E5L;
+	float coulombova_konst = 9.0e9F;
+
+	printf("Brzina svjetlosti u vakuumu iznosi %f, a tip long double zauzima %lu bajtova.\n", brz_svj_u_vakuumu, sizeof(long double));
+	printf("Coubumbova konstanta iznosi %f, a tip float zauzima %lu bajtova.", coulombova_konst, sizeof(float));
+
+	return 0;
+
+}
+*/
